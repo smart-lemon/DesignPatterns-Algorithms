b15356348f7fe24aa2efa1cda545558ba153dcd3
Samuel <siq.samuel@gmail.com>
2018-01-23 01:14:44 +0530

    Better comments -its 1am

--- Algorithms/Dynamic Programming/Knapsack.cpp
+++ Algorithms/Dynamic Programming/Knapsack.cpp
@@ -20,7 +20,7 @@ const static int C = 150;
 // Bottom up
 void knapsack_1_0() 
 { 
-    //v alue table having N + 1 rows and C + 1 columns
+    // DP array contains the best possible value -  having N + 1 rows and C + 1 columns
 	int DP[N + 1][C + 1];
 
 	// Row i = 0 with value 0 - because for weight 0, Result is 0
@@ -28,20 +28,20 @@ void knapsack_1_0()
 		DP[0][c] = 0;
 	}
 
-	// Column with i = 0 with value 0
+	// Column with c = 0 with value 0  - no capacity so you cant pack anything
 	for(int i = 0; i <= N; i++) {
 		DP[i][0] = 0;
 	}
 
 	// For every item
 	for(int i = 1; i <= N; i++) {
-        // incrementally changing the knapsack size 
+        // Incrementally changing the knapsack size 
 		for(int j = 1; j <= C; j++) {
-            // if a weight is more than the allowed weight, that weight cannot be picked.
-			if(weights[i] <= C) {
+            // If a weight is more than the allowed weight, that weight cannot be picked.
+			if(weights[i] <= j) {
                                 // i-th item is not used
 				DP[i][j] = max( DP[i - 1][j], 
-                                // i-th item is used
+                                // i-th item is used, j - weights[i] is the remaining capacity after usingg i-th item
                                 DP[i - 1][j - weights[i]] + values[i]); 
 			} else {
 				DP[i][j] = DP[i - 1][j];

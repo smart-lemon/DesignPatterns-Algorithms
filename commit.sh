d424b3a1b8cfe9e0850d7dcd4e98fa1792e5f097
Samuel <siq.samuel@gmail.com>
2018-03-18 20:30:55 +0530

    'testing frequent commits'

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 # Because I can't be bothered
+
+# Get the whole string that was given in the input
+str="'$*'"
+echo "$str"
+
+# Use the string as a commit message
 git add --all
 # Input argument (first)
-git commit -m $1
+git commit -m "$str"
 git push 

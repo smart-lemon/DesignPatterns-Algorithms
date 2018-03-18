1e60447604161b0c073607ef84b906961038e067
Samuel <siq.samuel@gmail.com>
2018-03-18 21:01:22 +0530

    'A very large sum'

--- Helper/HackerEarth.cpp
+++ Helper/HackerEarth.cpp
@@ -7,28 +7,12 @@
 using namespace std;
 
 
-vector<int> solve(int a0, int a1, int a2, int b0, int b1, int b2) {
-    vector<int> ar; 
-    int alice = 0, bob = 0;
-
-    if(a0 > b0)
-        alice++; 
-    else if (a0 < b0)
-        bob++;
-
-    if(a1 > b1)
-        alice++; 
-    else if (a1 < b1)
-        bob++;
-
-    if(a2 > b2)
-        alice++; 
-    else if (a2 < b2)
-        bob++;
-
-    ar.push_back(alice);
-    ar.push_back(bob);
-    return ar;
+long aVeryBigSum(int n, vector<long> ar) {
+   long sum = 0;
+    vector <long> :: iterator i;
+    for (i = ar.begin(); i != ar.end(); ++i)
+        sum += *i;
+    return sum;
 }
 
 

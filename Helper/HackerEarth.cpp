c1d03b618acb1147f503621f4ae254401c11545a
Samuel <siq.samuel@gmail.com>
2018-03-18 20:19:07 +0530

    hackerranksumbissiontest

--- Helper/HackerEarth.cpp
+++ Helper/HackerEarth.cpp
@@ -1,17 +1,34 @@
 #include "./../Include/CommonInc.h"
 
-
 #include <iostream> 
-#include <string> 
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int solveMeFirst(int x, int y) {
-    return x + y;
+int simpleArraySum(int n, vector<int> ar) {
+    int sum = 0;
+
+    vector <int> :: iterator i;
+
+    for (i = ar.begin(); i != ar.end(); ++i)
+        sum += *i;
+ 
+    return sum;
 }
 
-int tmain() 
-{
-   
-   return 0;
+
+int tmain() {
+
+    vector<int> ar(6);
+
+   for (int i = 1; i <= 5; i++)
+        ar.push_back(i);
+ 
+
+    int sum = simpleArraySum(6, ar);
+
+    cout << sum;
+
+    return 0;
 }
---
+++ commit.sh
@@ -0,0 +1,5 @@
+# Because I can't be bothered
+git add --all
+# Input argument (first)
+git commit -m $1
+git push 

501e6a12320ffad25069de1e75b9f958cd76447b
Samuel <siq.samuel@gmail.com>
2018-03-18 20:59:09 +0530

    'Alice bob problem'

--- Helper/HackerEarth.cpp
+++ Helper/HackerEarth.cpp
@@ -7,6 +7,30 @@
 using namespace std;
 
 
+vector<int> solve(int a0, int a1, int a2, int b0, int b1, int b2) {
+    vector<int> ar; 
+    int alice = 0, bob = 0;
+
+    if(a0 > b0)
+        alice++; 
+    else if (a0 < b0)
+        bob++;
+
+    if(a1 > b1)
+        alice++; 
+    else if (a1 < b1)
+        bob++;
+
+    if(a2 > b2)
+        alice++; 
+    else if (a2 < b2)
+        bob++;
+
+    ar.push_back(alice);
+    ar.push_back(bob);
+    return ar;
+}
+
 
 int tmain() {
 

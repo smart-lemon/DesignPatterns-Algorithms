458687610690eb2aefaae24a6c81a3654f70d7ad
Samuel <siq.samuel@gmail.com>
2018-04-15 21:51:27 +0530

    '.... why?'

--- Algorithms/Sorting/Quicksort.cpp
+++ Algorithms/Sorting/Quicksort.cpp
@@ -4,35 +4,31 @@ using namespace std;
 
 static int n;
 
-// Changed some thing and it is broken - TBD
+
 static int partition(int *data, int low, int high) {
 
-    int left = low + 1,  right = high,  pivot = data[low];
+    int left = low,  right = high,  pivot = data[low];
 
     while (left < right) {
 
         // Everthing on the left of pivot is lower than the pivot 
-        while ((left < right) && data[left] <= pivot) // <= is because left is the pivot initially
+        while ((left <= right) && data[left] <= pivot) // <= is because left is the pivot initially
             left++;
 
         // Everything on the right of the pivot is greater than the pivot 
-        while ((right > left) && data[right] > pivot)
+        while((left <= right) && data[right] > pivot)
             right--;
 
         if (left < right)
             swap(data, left, right);
-
-        cout << "P:" << pivot << " "; print1DArray(data, low + 1, high);  
     }
 
-    cout << "Full : "; print1DArray(data, n);
+    // Put the pivot in the 'rigthful' place
+    swap(data, low, right);
 
-    // Put the pivot in the 'rigthful' place between the left half and right half of the array
-    if(low != right - 1 )
-        swap(data, low, right - 1);
-
-    return right - 1;
+    return right;
 }
+
 
 // Quicksort 
 static void quick_sort(int *data, int low, int high)
@@ -47,7 +43,7 @@ static void quick_sort(int *data, int low, int high)
 
 void testQuickSort()
 {
-    int dataset[] = {0, 5, 4, -1, 8, 2, 6, 4, 3, 1, 7, 9, 1};
+    int dataset[] = {0, 5, 4, -1, 8, 2, 6, 4, 7, 1, 3, 9, -4};
 
     int low = 0, high = sizeof(dataset) / sizeof(dataset[0]);
     n = high;

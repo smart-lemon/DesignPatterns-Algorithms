ce08b032259beb44fe0b23b30c372ff37b0321f3
Samuel <siq.samuel@gmail.com>
2018-06-19 00:51:26 +0530

    'More bit utils'

--- Hack/CtCl/BitManipulation/BitUtilities.cpp
+++ Hack/CtCl/BitManipulation/BitUtilities.cpp
@@ -25,3 +25,20 @@ int clearBit(int num, int i) {
     int mask = ~(1 << i);
     return (num & mask);
 }
+
+/*
+    Clears MSB to i (including i)
+*/
+int clearBitFromMsbToI(int num, int i){
+    int mask = ~(1 << i) - 1;
+    return num & mask;
+}
+
+
+/*
+    Clears i to LSB (including i)
+*/
+int clearBitFromIToILsb(int num, int i){
+    int mask = (-1 << (i + 1));
+    return num & mask;
+}

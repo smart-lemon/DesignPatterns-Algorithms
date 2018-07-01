dc2aba3f7dfe044b4c263967e792f57c4241c4bf
Samuel <siq.samuel@gmail.com>
2018-07-01 12:37:24 +0530

    'TLE 312/313'

--- Prep/Leet/Arrays/Medium/ThreeSum.cpp
+++ Prep/Leet/Arrays/Medium/ThreeSum.cpp
@@ -1,27 +1,34 @@
+#include "./../../../../Include/Common.h"
+using namespace std;
+
+// https://leetcode.com/problems/3sum/description/
+
+// TLE 312/313 
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         unordered_map<int, int> hashmap; 
         
-        // Put all elements in an hashset
+        sort(nums.begin(), nums.end());
+     
         for(int i = 0; i < nums.size(); i++){
-             hashmap.insert(make_pair(nums[i], i));
+            hashmap.insert(make_pair(nums[i], i));
         }
         
-        set<vector<int>> outputSet; 
-        
+        set<vector<int>> outputSet;         
         for(int i = 0; i < nums.size(); i++){
             for(int j = i; j < nums.size(); j++){
                 if(i != j)
                 {
                     int sum = nums[i] + nums[j];
-                    if(hashmap.find(-sum) != hashmap.end())
-                    {
+                    if(!(hashmap.find(-sum) == hashmap.end()))
+                    {                       
                         if(hashmap[-sum] != i && hashmap[-sum] != j){
                             vector<int> line; 
                             line.push_back(nums[i]);
                             line.push_back(nums[j]);
-                            line.push_back(sum);
+                            line.push_back(-sum);
                             sort(line.begin(), line.end());
                             outputSet.insert(line);
                         }

bf148a622c9de5b886b0b865ff86971def83ce72
Samuel <siq.samuel@gmail.com>
2018-07-01 16:06:52 +0530

    'least common ancestor implementation'

--- Prep/Leet/Trees/Medium/LeastCommonAncestor.cpp
+++ Prep/Leet/Trees/Medium/LeastCommonAncestor.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -5,17 +7,11 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
- // Not yet solved 
-
 class Solution {
     
     TreeNode *recursiveLca(TreeNode* root, TreeNode* p, TreeNode* q){
         if(root == nullptr)
             return nullptr;
-        
-        if(root == p || root == q)
-            return root;
-        
         
         TreeNode *leftFound = recursiveLca(root->left, p, q);
         TreeNode *rightFound = recursiveLca(root->right, p, q);
@@ -23,11 +19,16 @@ class Solution {
         if(leftFound && rightFound)
             return root;
         
-        return nullptr; 
+        if(root == p || root == q)
+            return root;
+        
+        return leftFound? leftFound : rightFound; 
     }
     
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p == nullptr || q == nullptr)
+            return nullptr;
         return recursiveLca(root,  p, q);
     }
 };

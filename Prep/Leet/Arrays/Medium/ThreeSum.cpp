8d99b7459e25e0457dacfa9cad250e43350e8630
Samuel <siq.samuel@gmail.com>
2018-07-01 14:10:42 +0530

    '.. but this is accepted... WHY??'

--- Prep/Leet/Arrays/Medium/ThreeSum.cpp
+++ Prep/Leet/Arrays/Medium/ThreeSum.cpp
@@ -1,39 +1,40 @@
 #include "./../../../../Include/Common.h"
+
 using namespace std;
 
 // https://leetcode.com/problems/3sum/description/
 
 class Solution {
 public:
-    // TLE 312/313 -- HashMap solution which is based on a + b = -c
-    vector<vector<int>> threeSum_TLE(vector<int>& nums) {
-        unordered_map<int, int> hashmap; 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        unordered_map<int, int> hashmap;
+        set<vector<int>> outputSet;
         
         sort(nums.begin(), nums.end());
      
         for(int i = 0; i < nums.size(); i++){
-            hashmap.insert(make_pair(nums[i], i));
+            hashmap[nums[i]] = i;
         }
         
-        set<vector<int>> outputSet;         
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i; j < nums.size(); j++){
-                if(i != j)
-                {
-                    int sum = nums[i] + nums[j];
-                    if(!(hashmap.find(-sum) == hashmap.end()))
-                    {                       
-                        if(hashmap[-sum] != i && hashmap[-sum] != j){
-                            vector<int> line; 
-                            line.push_back(nums[i]);
-                            line.push_back(nums[j]);
-                            line.push_back(-sum);
-                            sort(line.begin(), line.end());
-                            outputSet.insert(line);
-                        }
+        int i = 0, j = i + 1;
+        while(i < nums.size()){
+            while(j < nums.size()){
+                int sum = nums[i] + nums[j];
+                if(!(hashmap.find(-sum) == hashmap.end()))
+                {                       
+                    if(hashmap[-sum] != i && hashmap[-sum] != j){
+                        vector<int> line; 
+                        line.push_back(nums[i]);
+                        line.push_back(nums[j]);
+                        line.push_back(-sum);
+                        sort(line.begin(), line.end());
+                        outputSet.insert(line);
                     }
                 }
+                j = hashmap[nums[j]] + 1;
             }
+            i = hashmap[nums[i]] + 1;
+            j = i + 1;
         }
         
         vector<vector<int>> output;
@@ -41,6 +42,7 @@ public:
         for( it = outputSet.begin(); it != outputSet.end(); ++it){
              output.push_back(*it);
         }
+
         return output;
     }
 };

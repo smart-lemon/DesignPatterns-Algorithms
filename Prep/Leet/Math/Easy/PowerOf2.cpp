4ad2767365c472f286aa99f7e350fe684a9ff639
Samuel <siq.samuel@gmail.com>
2018-07-06 01:50:42 +0530

    'Power of 2 problem'

---
+++ Prep/Leet/Math/Easy/PowerOf2.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/power-of-two/description/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0)
+            return false;
+        
+        while(n % 2 == 0){
+            n = n/2;
+        }
+
+        if(n == 1)
+            return true;
+        return false; 
+    }
+};

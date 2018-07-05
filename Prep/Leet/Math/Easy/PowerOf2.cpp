3e081bc82fbdf34cf6f04cb184918d5d44591418
Samuel <siq.samuel@gmail.com>
2018-07-06 01:59:53 +0530

    'Power of 2 problem -- Added better O(1) implementation'

--- Prep/Leet/Math/Easy/PowerOf2.cpp
+++ Prep/Leet/Math/Easy/PowerOf2.cpp
@@ -10,8 +10,16 @@ public:
             n = n/2;
         }
 
-        if(n == 1)
-            return true;
-        return false; 
+        return (n == 1);
     }
+
+    // Pay attention to  0 and power of two means only one bit is 1
+    bool isPowerOfTwoBetter(int n) {
+        return n > 0 && !(n & (n - 1));
+    }
+    /*
+        n = 2 ^ 0 = 1 = 0b0000...00000001, and (n - 1) = 0 = 0b0000...0000.
+        n = 2 ^ 1 = 2 = 0b0000...00000010, and (n - 1) = 1 = 0b0000...0001.
+        n = 2 ^ 2 = 4 = 0b0000...00000100, and (n - 1) = 3 = 0b0000...0011.
+    */
 };

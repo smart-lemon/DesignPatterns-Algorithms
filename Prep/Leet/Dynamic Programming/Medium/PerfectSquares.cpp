517c4ad35d013097dac09055f39deb7d39038d91
Samuel <siq.samuel@gmail.com>
2018-07-06 01:34:03 +0530

    ''

---
+++ Prep/Leet/Dynamic Programming/Medium/PerfectSquares.cpp
@@ -0,0 +1,43 @@
+
+// https://leetcode.com/problems/perfect-squares/description/
+
+
+class Solution {
+    
+    int min(int a, int b){
+        return a < b? a : b;
+    }
+public:
+    int numSquares(int n) {
+        vector<int> squares;
+        
+        if(n <= 1)
+            return n;
+        int cnt = 0;
+        for(int i = 1; i < n; i++) {
+            if(i*i > n)
+                break;
+            else {                
+                squares.push_back(i*i);
+                cout << squares[cnt] << " "; 
+                cnt++;
+            }
+        }
+        cout << endl;
+        
+        vector<int> DP(n + 1, 0);
+        DP[0] = 0; 
+        DP[1] = squares[0];
+        
+        for(int i = 2; i <= n; i++) {
+            int minOf = INT_MAX; 
+            for(int j = 0; j < cnt; j++) {
+                if(i - squares[j] >= 0){
+                    minOf = min(DP[i - squares[j]] + 1, minOf); 
+                }
+            }
+            DP[i] = minOf;
+        }
+        return DP[n];
+    }
+};
--- Prep/Leet/Strings/Medium/ValidateIp--NotDone.cpp
+++ Prep/Leet/Strings/Medium/ValidateIp--NotDone.cpp
@@ -19,14 +19,18 @@ public:
     bool validateIpv4Block(string block) {
         int len = block.length();
         // 1. If first char is zero rest are not char. 
+        if(block[0] == '0' && len > 1)
+            return false;
+
         int num = 0, numberCnt = 0;
         for(int i = 0; i < len; i++){
             int it = block[i] - '0'; 
             num = (num * 10) + it;
             numberCnt++;
+            
             if(numberCnt > 3){
-                    cout << "More than 3 numbers " << endl;
-                    return false;
+                cout << "More than 3 numbers " << endl;
+                return false;
             }
         // if the 
         }
@@ -35,6 +39,7 @@ public:
             cout << "The number is more than 255 -> " << num << endl;
             return false; 
         }
+        return true;
     }
     
     bool validateIpv4(string arr){
@@ -44,7 +49,9 @@ public:
         int next = arr.find(".");
         while(next != string::npos){
             string block = arr.substr(start, next);
-            validateIpv4Block(block);
+            bool ret = validateIpv4Block(block);
+            if(ret == false)
+                return false;
             start = next + 1;
             next = arr.find(".");
         }
@@ -57,7 +64,7 @@ public:
     
     bool validateIpv6Block(string arr){
         int len = arr.length(); 
-        
+        string validIp6Str = "abcdefABCDEF";
         for(int i = 0; i < len; i++){
             
         }

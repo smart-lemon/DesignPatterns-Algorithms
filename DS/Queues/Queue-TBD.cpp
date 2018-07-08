c37979fc4b92bf70fcf4d117d6808a2017221737
Samuel <siq.samuel@gmail.com>
2018-07-08 19:59:14 +0530

    'TLE, improving solution'

--- DS/Queues/Queue-TBD.cpp
+++ DS/Queues/Queue-TBD.cpp
@@ -32,6 +32,7 @@ class Queue {
             return;
         }
 
+        
 
     }
      
---
+++ Prep/Leet/Arrays/Medium/ContainsDuplicate-iii.cpp
@@ -0,0 +1,27 @@
+#include "./../../../../Include/Common.h"
+using namespace std;
+
+// https://leetcode.com/problems/contains-duplicate-iii/description/
+
+class Solution {
+    
+    long difference(long a, long b){
+        return a > b? (a - b) : (b - a);
+    }
+public:
+    // 40/41 sets 
+    bool containsNearbyAlmostDuplicateTle(vector<int>& nums, int k, int t) {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j <= i + k && j <  nums.size(); j++) {
+                long diff = 0;
+                cout << diff << endl; 
+
+                diff = difference(nums[i], nums[j]);
+                if(abs(diff) <= t)
+                    return true;
+            }
+        }
+        return false;
+    }
+};

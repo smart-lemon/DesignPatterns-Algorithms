1c857b6b25fdc6f57f4aeedf977d2c8305b77e16
Samuel <siq.samuel@gmail.com>
2018-07-14 13:36:01 +0530

    'Racing thru firecode'

---
+++ Prep/FireCode/L1/Fibonacci.cpp
@@ -0,0 +1,14 @@
+// namespace std have been included for this problem.
+
+// Add any helper functions(if needed) above.
+int fib(int n)
+{
+    int *fibo = new int[n + 1]; 
+    fibo[0] = 0;
+    fibo[1] = 1; 
+    
+    for(int i = 2; i <= n; i++){
+        fibo[i] = fibo[i - 1] + fibo[i - 2]; 
+    }
+    return fibo[n]; 
+}

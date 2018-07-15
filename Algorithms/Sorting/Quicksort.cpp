3afbfc4c2a132aa30211ce691e3164294506770d
Reno <siq.samuel@gmail.com>
2018-07-15 23:51:58 +0530

    ''

--- Algorithms/Sorting/Quicksort.cpp
+++ Algorithms/Sorting/Quicksort.cpp
@@ -48,42 +48,13 @@ static void quick_sort(int *data, int low, int high)
 }
 
 
-void quicksort(int *data,int first,int last){
-   int right, left, pivot_pos, temp;
-
-   if(first < last){
-        pivot_pos =  first;
-        right = first;
-        left = last;
-
-        while(left < right) {
-
-            while(data[left] <= data[pivot_pos] && left < last)
-                left++;
-
-            while(data[right] > data[pivot_pos])
-                right--;
-
-            if(left < right){
-                swap(data, left, right);
-            }
-        }
-
-        swap(data, pivot_pos, right);
-
-        quicksort(data, first, right - 1);
-        quicksort(data, right + 1, last);
-   }
-}
-
-
 void testQuickSort()
 {
     int dataset[] = {0, -2, -3, -1, -4, -2, -1};
 
     int low = 0, high = sizeof(dataset) / sizeof(dataset[0]);
 
-    quicksort(dataset, low, high - 1);
+    quick_sort(dataset, low, high - 1);
 
     cout << "Quick sort: ";
     print1DArray(dataset, high);

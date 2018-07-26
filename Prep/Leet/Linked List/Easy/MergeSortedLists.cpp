29a31629ce0a8806d9fde031566182ce4dddb19c
Reno <siq.samuel@gmail.com>
2018-07-26 23:24:48 +0530

    'Merge Sorted List is accepted'

--- Prep/Leet/Linked List/Easy/MergeSortedLists.cpp
+++ Prep/Leet/Linked List/Easy/MergeSortedLists.cpp
@@ -1,33 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+/*
+    21. https://leetcode.com/problems/merge-two-sorted-lists/description/ 
  */
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+ };
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *runner1 = l1, *runner2 = l2; 
-        ListNode *outputHead = nullptr, *outputPrev = new ListNode(0), *outputRunner = nullptr;
+        ListNode *outputHead = nullptr, *outputRunner = nullptr;
+        ListNode *outputPrev = new ListNode(0); // dummy
         
         if(l1 == nullptr && l2 == nullptr)
             return nullptr;
         
+        // The head is initially a dummy 
+        outputHead = outputPrev;
+        
         while(runner1 && runner2) {
             if(runner1->val < runner2->val) {
-                cout << "R1 < R2 " << runner1->val << " " << runner2->val << " ";
                 outputRunner = new ListNode(runner1->val);
                 outputPrev->next = outputRunner;
                 runner1 = runner1->next;
             } else if(runner1->val > runner2->val) {
-                cout << "R1 > R2 " << runner1->val << " " << runner2->val << " ";
                 outputRunner = new ListNode(runner2->val);
                 outputPrev->next = outputRunner;
                 runner2 = runner2->next;
             } else if(runner1->val == runner2->val) {
-                cout << "Equal " << runner1->val << " " << runner2->val << " ";
                 outputRunner = new ListNode(runner1->val);
                 outputPrev->next = outputRunner;
                 outputPrev = outputPrev->next;
@@ -37,25 +41,17 @@ public:
                 runner2 = runner2->next;
             }
             
-            if(outputHead == nullptr) {
-                outputHead = outputPrev;
-            }
             outputPrev = outputPrev->next;
-        }
+        }       
         
         if(runner1) {
-           if(outputHead == nullptr) {
+            if(outputHead->next == nullptr)
                 return runner1;
-            }
-           cout << "Append " << runner1->val << " ";
            outputPrev->next = runner1;
         }
         if(runner2) {
-           if(outputHead == nullptr) {
+           if(outputHead->next == nullptr)
                 return runner2;
-           }
-           cout << "Append " << runner2->val << " ";
-
            outputPrev->next = runner2;
         }
         

eb7be47f3ba71c04d3dc357caa83c959c08e647e
Reno <siq.samuel@gmail.com>
2018-08-01 23:43:48 +0530

    'N Squared algorithm'

--- Prep/Leet/Dynamic Programming/Medium/ContigousSubarraySum.cpp
+++ Prep/Leet/Dynamic Programming/Medium/ContigousSubarraySum.cpp
@@ -5,7 +5,26 @@
 
 class Solution {
 public:
-    bool checkSubarraySum(vector<int>& nums, int k) {
+    bool checkSubarraySumOrderNSquared(vector<int>& nums, int k) {
+        int *presum = new int[nums.size() + 1]; 
+        presum[0] = 0; 
         
+        // Precalculate sums 
+        for(int i = 1; i <= nums.size(); i++){
+            presum[i] = presum[i - 1] + nums[i - 1];
+        }
+
+        for(int i = 0; i <= nums.size(); i++){
+            // At least two numbers must be selected
+            for(int j = i + 2; j <= nums.size(); j++){            
+                if(k == 0){
+                    if(presum[j] == 0 && presum[i] == 0)
+                        return true;
+                } else if((presum[j] - presum[i]) % k == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };

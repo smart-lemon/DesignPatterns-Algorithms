ca6d8efd8a26acceeed8dddb8fa71ad49e85732b
Reno <siq.samuel@gmail.com>
2018-08-02 22:29:10 +0530

    'Just be very greedy for this algo'

---
+++ Prep/FireCode/L3/StockMarket.cpp
@@ -0,0 +1,21 @@
+
+//  Max Profit in a Stock Market 
+int max_profit(int prices[], int sz)
+{
+    int min_index = 0; 
+    int best = 0;
+    
+    int profits = 0;
+    
+    int i = 1; 
+    while (i < sz) {
+        if(prices[i] > prices[i - 1]){
+            int diff = prices[i] - prices[i - 1];
+            profits += diff; 
+        }
+        i++;
+    }
+    
+ 
+    return profits;
+}

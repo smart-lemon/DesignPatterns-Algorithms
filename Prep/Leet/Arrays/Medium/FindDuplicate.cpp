82b8dce33c07ee07a22b16aef07e4784df70e26c
Reno <siq.samuel@gmail.com>
2018-08-05 14:23:58 +0530

    'The real O(N) algorithm'

--- Prep/Leet/Arrays/Medium/FindDuplicate.cpp
+++ Prep/Leet/Arrays/Medium/FindDuplicate.cpp
@@ -4,21 +4,44 @@
 
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+
+    // N-squared approach
+    int findDuplicateNdsquared(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int sz = nums.size();
         int output = -1;
-        bool flag = false;
+        bool found_flag = false;
+
         for(int i = 1; i < sz; i++){
             while(nums[i] == nums[i-1]){
                 i++;
-                flag = true;
+                found_flag = true;
             }
-            if(flag)
+            if(found_flag)
             {
                 output = nums[i - 1];
                 return output;
             }
         }
     }
+
+    // Explanation 
+    // http://keithschwarz.com/interesting/code/?dir=find-duplicate
+
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+        int find = 0;
+        while(slow != find){
+            slow = nums[slow];
+            find = nums[find];
+        }
+        return find;
+    }
+
 };

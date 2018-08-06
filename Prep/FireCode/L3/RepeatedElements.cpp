1a5275f58a97eb0c3ce62911acf14c8a08174f09
Reno <siq.samuel@gmail.com>
2018-08-07 00:31:34 +0530

    'O(n) algorithm'

--- Prep/FireCode/L3/RepeatedElements.cpp
+++ Prep/FireCode/L3/RepeatedElements.cpp
@@ -1,68 +1,18 @@
 // namespace std have been included for this problem.
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>
 
-void swap(int *numbers, int a, int b){
-    int temp = numbers[a];
-    numbers[a] = numbers[b];
-    numbers[b] = temp;
-}
-
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[low];
-    int left = low - 1, right = high + 1;
- 
-    while (true) {
-         do {
-            left++;
-        } while (arr[left] < pivot);
- 
-        do {
-            right--;
-        } while (arr[right] > pivot);
- 
-        // If two pointers met.
-        if (left >= right)
-            return right;
- 
-        swap(arr, left, right);
-    }
-}
- 
-// Generates Random Pivot
-int partition_r(int arr[], int low, int high)
-{
-    // Generate a random number in between
-    // low .. high
-    srand(time(NULL));
-    int random = low + rand() % (high - low);
- 
-    // Swap A[random] with A[high]
-    swap(arr, random, low);
- 
-    return partition(arr, low, high);
-}
- 
-void quickSort(int *arr, int low, int high)
-{
-    if (low < high) {
-
-        int pi = partition_r(arr, low, high);
- 
-        quickSort(arr, low, pi);
-        quickSort(arr, pi + 1, high);
-    }
-}
 // Add any helper functions(if needed) above.
 int* remove_dup(int A[], int sz)
 {    /* Alocate the memory of Output array of maximum size n. 
     Fill the new array with unique elements, which have duplicates entries in input array 
     Size of Output array will be less than n. */
     int *output = nullptr;
+
+    if(sz == 1)
+        return output;
+
     // Add your code below this line. Do not modify any other code.
-    quickSort(A, 0, sz - 1);
-    
+    sort(A, A + sz);
+
     int k = 0; 
     output = new int[sz];
     bool flag = false;
@@ -79,11 +29,6 @@ int* remove_dup(int A[], int sz)
         }
     }
     
-    
-    for(int i = 0; i < k; i++){
-        cout << output[i] << " "; 
-    }
-    cout << "===" << endl;
     // Add your code above this line. Do not modify any other code.
     return output;
     
--- Prep/Leet/Arrays/Medium/FindAllDuplicates.cpp
+++ Prep/Leet/Arrays/Medium/FindAllDuplicates.cpp
@@ -3,6 +3,8 @@
 
 class Solution {
 public:
+
+    // O(N^2) 
     vector<int> findDuplicates(vector<int>& nums) {
          sort(nums.begin(), nums.end());
          
@@ -20,5 +22,19 @@ public:
             }
          }  
         return output;
+    }
+
+    // O(N)
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] != nums[nums[i] - 1]) swap(nums[i], nums[nums[i]-1]);
+            else i++;
+        }
+        for (i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1) res.push_back(nums[i]);
+        }
+        return res;
     }
 };

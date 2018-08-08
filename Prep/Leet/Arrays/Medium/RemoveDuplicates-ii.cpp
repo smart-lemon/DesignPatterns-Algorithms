e9bfd34ec652e14593e8c06d844859cd27d93752
Reno <siq.samuel@gmail.com>
2018-08-09 01:26:48 +0530

    'Remove duplicates impl' '

---
+++ Prep/Leet/Arrays/Medium/RemoveDuplicates-ii.cpp
@@ -0,0 +1,21 @@
+#include "./../../../../Include/Common.h"
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        if(nums.size() <= 2)
+            return nums.size();
+        int rear = 1; 
+
+        for(int i = 2; i < nums.size(); i++){
+            if(!(nums[i] == nums[rear] && nums[i] == nums[rear - 1]))
+            {
+                nums[++rear] = nums[i];
+            }
+        }
+        return rear + 1;
+    }
+};

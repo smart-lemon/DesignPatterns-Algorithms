87f4fc4c031c9da06721312be4ad59a1d94e999b
Reno <siq.samuel@gmail.com>
2018-08-21 00:09:43 +0530

    'New problem'

---
+++ Prep/FireCode/L3/LinkedListInsertAtPos-TBD.cpp
@@ -0,0 +1,10 @@
+
+typedef struct listNode{
+      int value;               /* value of the node */
+      struct listNode* next;   /* points to the next linked-list node */
+};
+
+listNode* insert_at_position( listNode* head, int data, int pos)
+{
+    
+}

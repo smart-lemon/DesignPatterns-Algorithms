18a1e4768d43d8d8f3f597d94ddb04d564338d64
Reno <siq.samuel@gmail.com>
2018-08-23 01:15:51 +0530

    'Adding new firecode problem'

---
+++ Prep/FireCode/L3/RangeModule-TBD.cpp
@@ -0,0 +1,20 @@
+#include "./../../../Include/Common.h"
+
+class Interval {
+    public:
+        int start;
+        int end;
+        Interval(){}
+        Interval(int start, int end) {
+            this->start = start;
+            this->end = end;
+        }
+};
+
+
+// Add any helper functions(if needed) above.
+vector<Interval> merge_intervals(vector<Interval> intervals_list)
+{
+    
+    
+}
--- Prep/FireCode/L3/RepeatedElements.cpp
+++ Prep/FireCode/L3/RepeatedElements.cpp
@@ -1,10 +1,12 @@
-// namespace std have been included for this problem.
+#include "./../../../Include/Common.h"
 
-// Add any helper functions(if needed) above.
+
 int* remove_dup(int A[], int sz)
-{    /* Alocate the memory of Output array of maximum size n. 
-    Fill the new array with unique elements, which have duplicates entries in input array 
-    Size of Output array will be less than n. */
+{    /* 
+        Instructions : Alocate the memory of Output array of maximum size n. 
+        Fill the new array with unique elements, which have duplicates entries in input array 
+        Size of Output array will be less than n. 
+    */
     int *output = nullptr;
 
     if(sz == 1)
@@ -15,6 +17,7 @@ int* remove_dup(int A[], int sz)
 
     int k = 0; 
     output = new int[sz];
+
     bool flag = false;
     for(int i = 1; i < sz; i++){
         while(A[i] == A[i-1]){

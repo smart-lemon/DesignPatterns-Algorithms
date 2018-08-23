573fb7c73ec99d3b2ecaf477cff46f13689b4548
Reno <siq.samuel@gmail.com>
2018-08-24 00:32:27 +0530

    'Comment about Leet and Fire difference'

--- Prep/FireCode/L3/LinkedListFindMiddleNode.cpp
+++ Prep/FireCode/L3/LinkedListFindMiddleNode.cpp
@@ -9,6 +9,7 @@ listNode* find_middle_node(listNode* head)
 {
     listNode *fast = head, *slow = head;
     
+    // This is different for Leetcode, it expects the floor node of the middle in case of even 
     while(fast && fast->next && fast->next->next) { 
         fast = fast->next->next;
         slow = slow->next; 

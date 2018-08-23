edb224babc5a7cf8c9e589c174e3141086c67344
Reno <siq.samuel@gmail.com>
2018-08-24 00:17:58 +0530

    'Partial pass of Find Middle element'

---
+++ Prep/FireCode/L3/LinkedListFindMiddleNode.cpp
@@ -0,0 +1,18 @@
+
+typedef struct listNode{
+      int value;               /* value of the node */
+      struct listNode* next;   /* points to the next linked-list node */
+};
+
+
+listNode* find_middle_node(listNode* head)
+{
+    listNode *fast = head, *slow = head;
+    
+    while(fast && fast->next) { 
+        fast = fast->next->next;
+        slow = slow->next; 
+    }
+
+    return slow;
+}

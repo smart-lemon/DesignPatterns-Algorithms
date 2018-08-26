ab39519142af4adc216a367958efd7638bff0e17
Reno <siq.samuel@gmail.com>
2018-08-26 21:06:48 +0530

    'Adding the firecode submission for posterity'

--- Prep/FireCode/L3/ReverseLinkedList.cpp
+++ Prep/FireCode/L3/ReverseLinkedList.cpp
@@ -3,8 +3,19 @@ typedef struct listNode{
       struct listNode *next;   /* points to the next linked-list node */
 };
 
-listNode* reverse_list(listNode *head)
+listNode* reverse_list(listNode* head)
 {
+    listNode *previous, *current, *ahead; 
     
+    previous = nullptr; 
+    current = head; 
     
+    while(current) {
+        ahead = current->next; 
+        current->next = previous; 
+        previous = current; 
+        current = ahead; 
+    }
+    
+    return previous;
 }

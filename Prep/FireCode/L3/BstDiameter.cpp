c634087e54237e89e126edfc57b6e1a9c6275d8a
Reno <siq.samuel@gmail.com>
2018-08-26 19:12:46 +0530

    'Pass on firecode'

--- Prep/FireCode/L3/BstDiameter.cpp
+++ Prep/FireCode/L3/BstDiameter.cpp
@@ -6,6 +6,7 @@ struct treeNode{
 
 static int max(int a, int b) { return (a > b)? a : b; }
 
+// Not used in this program 
 int find_depth(treeNode *node){
     if(node == nullptr){
         return 0; 
@@ -14,10 +15,28 @@ int find_depth(treeNode *node){
     return max(find_depth(node->right), find_depth(node->left)) + 1;
 }
 
+// A longest path or route between any two nodes in a tree. 
+// The path may or may not go through the root.
+int diameterOpt(treeNode *root, int *height) {
+    
+    if(root == nullptr) {
+        *height = 0;
+        return 0;
+    }
+    int left_ht  = 0; 
+    int right_ht = 0;
+    
+    int left_dia  = diameterOpt(root->left,  &left_ht);
+    int right_dia = diameterOpt(root->right, &right_ht);
+    
+    // Height or depth is the max of the left or right subtree depth + 1
+    *height = max(left_ht, right_ht) + 1;
+
+    return max(left_ht + right_ht + 1, max(left_dia, right_dia));
+}
+
 int diameter(treeNode* root)
 {
-    if(root == nullptr)
-        return 0; 
-    return find_depth(root->left) + find_depth(root->right) + 1;    
-    
+    int height = 0; 
+    return diameterOpt(root, &height);
 }

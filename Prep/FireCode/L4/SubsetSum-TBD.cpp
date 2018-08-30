d115bcde52c3f777120cc8aedbdd2f0b2b21d426
Reno <siq.samuel@gmail.com>
2018-08-31 01:26:56 +0530

    'No dice. Yet.'

--- Prep/FireCode/L4/SubsetSum-TBD.cpp
+++ Prep/FireCode/L4/SubsetSum-TBD.cpp
@@ -3,10 +3,39 @@
     such that the sum of all numbers in the subset is equal to a given target.
 */
 
-// Note to self: Not yet handled negative numbers and it seems i have to get arr sorted first
+
+
+// Swap position a with b in an array of integer numbers
+void swap(int *numbers, int a, int b){
+    int temp = numbers[a];
+    numbers[a] = numbers[b];
+    numbers[b] = temp;
+}
+
+void bubble_sort(int *data, int n){
+
+   bool swapped = true;
+
+   // No more passes required if swapped remains false
+   for(int pass = n - 1; pass >= 0 && swapped; pass--){
+
+       swapped = false;
+       for(int i = 0; i <= pass - 1 ; i++)
+       {
+           if(data[i] > data[i + 1]){
+               swap(data, i, i + 1);
+               swapped = true; 
+           }
+       }
+   }
+}
+
+
+// Note to self: Not yet handled negative numbers 
 bool group_sum_with_num(int arr[], int sz, int must_have, int target_sum)
 {
     int target = target_sum; 
+    bubble_sort(arr, sz);
     bool **DP;
     DP = new bool*[sz];
     for(int i = 0; i < sz; i++)

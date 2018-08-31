00b7aca8570422fafe05d7aa3592c4e56a0785a9
Reno <siq.samuel@gmail.com>
2018-08-31 23:03:34 +0530

    'Simplified code for Plus one'

--- Prep/Leet/Arrays/Easy/PlusOne.cpp
+++ Prep/Leet/Arrays/Easy/PlusOne.cpp
@@ -15,16 +15,28 @@ public:
             if(digits[i] > 9){
                 digits[i - 1] += 1;
                 digits[i] = 0;
+            } else {
+                break;
             }
         }
 
         // insert at 1 at front
         if(digits[0] > 9){
             digits[0] = 0;
-            vector<int>::iterator it;
-            it = digits.begin();
-            digits.insert(it, 1);   
+            digits.insert(digits.begin(), 1);   
         }
         return digits;
     }
 };
+
+vector<int> plusOneShorter(vector<int>& digits) {
+    for(int i = digits.size() - 1; i >= 0; i--) {
+        if (++digits[i] %= 10)
+            return digits;
+    }
+    
+    digits[0] = 1;
+    digits.push_back(0);
+
+    return digits;
+}

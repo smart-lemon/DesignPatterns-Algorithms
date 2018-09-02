3bf33ff5495b6cb3271eda9db3f0347a8469fd9d
Reno <siq.samuel@gmail.com>
2018-09-02 23:54:13 +0530

    'Setting up what's updog'

--- Prep/Leet/Arrays/Medium/DiagonalTraversal-TBD.cpp
+++ Prep/Leet/Arrays/Medium/DiagonalTraversal-TBD.cpp
@@ -1,6 +1,30 @@
+#include "./../../../../Include/Common.h"
+
+/* 
+    498. Diagonal traversal 
+    https://leetcode.com/problems/diagonal-traverse/description/ 
+*/
+
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-        
+        int R = 0, C = 0;
+        vector<int> ans; 
+
+        enum dir {
+            UP = true,
+            DOWN = false
+        };
+
+        dir direction = UP;
+
+        // The dimensions
+        R = matrix.size();
+        C = matrix[0].size();
+
+        while(ans.size() == R * C) {
+            
+        }
     }
 };

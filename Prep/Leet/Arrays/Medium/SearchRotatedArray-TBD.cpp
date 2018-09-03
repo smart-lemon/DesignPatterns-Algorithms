4d7cf0628fabffa0eb62ae63d228ac577b876ed2
Reno <siq.samuel@gmail.com>
2018-09-04 00:35:29 +0530

    '193 / 196 test cases passed.'

--- Prep/Leet/Arrays/Medium/SearchRotatedArray-TBD.cpp
+++ Prep/Leet/Arrays/Medium/SearchRotatedArray-TBD.cpp
@@ -8,6 +8,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int l = 0, m, r = nums.size() - 1;
+        int key = target;
+
+        while(l <= r) {
+            m = (l + r) / 2;
+
+            // Check if m is the solution
+            if(nums[m] == target)
+                return m;
+
+            // Bottom half is sorted. Inculde l, as m is already checked
+            if(nums[l] < nums[m]){
+                if(nums[l] <= key && key < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            // Upper half is sorted. Inculde r, as m is already checked
+            } else {
+                if(nums[m] < key && key <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+        }
+
+        return - 1;
     }
 };
+
+// Details : https://articles.leetcode.com/searching-element-in-rotated-array/

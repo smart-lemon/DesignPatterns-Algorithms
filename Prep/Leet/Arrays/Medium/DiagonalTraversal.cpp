ad83574c891691c0fcbe89953c071e91affd0b10
Reno <siq.samuel@gmail.com>
2018-09-04 00:22:18 +0530

    'Added comments for better undestanding'

--- Prep/Leet/Arrays/Medium/DiagonalTraversal.cpp
+++ Prep/Leet/Arrays/Medium/DiagonalTraversal.cpp
@@ -38,10 +38,10 @@ public:
                     c++;
                 }
                 // Shift right or down
-                r++; // Compenstaion for r-- 
-                c--;
-                (c < C - 1) ? c++ : r++;
+                r++; c--; // Compenstaion for overshooting
                 
+                // Move right if c < C - 1 else move down 
+                (c < C - 1) ? c++ : r++;
             } else {
                 while(boundsCheck(r, c, R, C)){
                     // Slide down diagonally                   
@@ -49,14 +49,44 @@ public:
                     r++;
                     c--;
                 }
-                // Shift right or down
-                r--; c++;
-                (r < R-1) ? r++ : c++;
+                r--; c++; // // Compenstaion for overshooting
 
+                // Move down if r < R - 1 else move right 
+                (r < R - 1) ? r++ : c++;
             }
-
+            // Change directions
             direction == UP? direction = DOWN : direction = UP;
         }
         return ans;
+    }
+    
+    
+    vector<int> findDiagonalOrderAlternative(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if (m == 0) return vector<int>();
+        int n = matrix[0].size();
+        
+        // Number of diagonals are m + n - 1
+        vector<vector<int>> tmp (m + n - 1);
+        
+        // Put all diagonal sequences from top-right to bottom-left to an array
+        for (int i = 0; i < m + n - 1 ; i++) {
+            int row = max(0, i - n + 1);
+            int col = min(i, n - 1);
+            for (; col >= 0 && row < m; row++, col--) {
+                tmp[i].push_back(matrix[row][col]);
+            }
+        }
+
+        // Combine all sequence together by reversing odd sequences.
+        vector<int> res;
+        for (int i = 0; i < tmp.size(); i++) {
+            if (i % 2) 
+                res.insert(res.end(), tmp[i].begin(), tmp[i].end());
+            else 
+                res.insert(res.end(), tmp[i].rbegin(), tmp[i].rend());
+        }
+
+        return res;
     }
 };

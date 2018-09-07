617692555fbf835a8e994bd17e1d1abdf0662b14
Reno <siq.samuel@gmail.com>
2018-09-08 02:11:44 +0530

    'A faster 1D implementation of coin change'

--- Prep/Leet/Dynamic Programming/Medium/CoinChangeNoOfWays-TBD.cpp
+++ Prep/Leet/Dynamic Programming/Medium/CoinChangeNoOfWays-TBD.cpp
@@ -6,21 +6,20 @@ public:
     int change(int amount, vector<int>& coins) {
         // vector<vector<int>> vec(ROW_NUMBER, vector <int> (COL_NUMBER, DEFAULT_VALUE))  
         
-        if(coins.size() == 0 && amount == 0)
+        if(amount == 0)
             return 1;
-        else if(coins.size() == 0)
+        if(coins.size() == 0)
             return 0;
 
         int cols = amount + 1, rows = coins.size();
         vector<vector<int>> dp(rows, vector <int>(cols, 0));
 
         // There is one way to make the amount 0, using no coins
-        for(int r = 0; r < rows; r++)
-            dp[r][0] = 1;
+        dp[0][0] = 1;
         
         int top = 0, left = 0;
         for(int r = 0; r < rows; r++){
-            for(int c = 1; c < cols; c++){
+            for(int c = 0; c < cols; c++){
                 top = 0, left = 0;
                 if(r - 1 >= 0) 
                     top = dp[r - 1][c]; 
@@ -31,5 +30,20 @@ public:
             }
         }
         return dp[rows - 1][cols - 1];
+    }
+
+    int changeReducedTo1D(int amount, vector<int>& coins) {
+        if(amount == 0)
+            return 1;
+        if(coins.size() == 0)
+            return 0;
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); ++ i){
+            for(int j = coins[i]; j <= amount; ++ j){
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
     }
 };

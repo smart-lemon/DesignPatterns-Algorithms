0f03cb1b581804dbe7a27fa713442cc6bf201509
Reno <siq.samuel@gmail.com>
2018-09-09 14:01:57 +0530

    'Coin change completed'

--- Prep/Leet/Dynamic Programming/Medium/CoinChangeNoOfWays.cpp
+++ Prep/Leet/Dynamic Programming/Medium/CoinChangeNoOfWays.cpp
@@ -1,11 +1,19 @@
 #include "./../../../../Include/Common.h"
 
+/*
+    518. Coin change 2 - You are given coins of different denominations and a total amount of money. 
+    Write a function to compute the number of combinations that make up that amount. 
+
+    https://leetcode.com/problems/coin-change-2/ 
+
+*/
 
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+
+        // Allocating an STL 2-D array
         // vector<vector<int>> vec(ROW_NUMBER, vector <int> (COL_NUMBER, DEFAULT_VALUE))  
-        
         if(amount == 0)
             return 1;
         if(coins.size() == 0)
@@ -15,11 +23,12 @@ public:
         vector<vector<int>> dp(rows, vector <int>(cols, 0));
 
         // There is one way to make the amount 0, using no coins
-        dp[0][0] = 1;
+        for(int r = 0; r < rows; r++)
+            dp[r][0] = 1;
         
         int top = 0, left = 0;
         for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
+            for(int c = 1; c < cols; c++){
                 top = 0, left = 0;
                 if(r - 1 >= 0) 
                     top = dp[r - 1][c]; 
--- Prep/Leet/Dynamic Programming/Medium/ContigousSubarraySum-TBD.cpp
+++ Prep/Leet/Dynamic Programming/Medium/ContigousSubarraySum-TBD.cpp
@@ -1,7 +1,10 @@
 #include "./../../../../Include/Common.h"
-
-// 523 . 
-// https://leetcode.com/problems/continuous-subarray-sum/
+/*
+    523 .  Given target integer k, write a function to check if the array has a continuous subarray of size 
+    at least 2 that sums up to the multiple of k
+    
+    https://leetcode.com/problems/continuous-subarray-sum/
+*/
 
 class Solution {
 public:

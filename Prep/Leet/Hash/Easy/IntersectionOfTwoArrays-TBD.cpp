46964b7bd800d5e6aff76dd3a901938dc2ce26a2
Reno <siq.samuel@gmail.com>
2018-09-19 23:30:05 +0530

    'Intersection of arrays'

--- Prep/Leet/Hash/Easy/IntersectionOfTwoArrays-TBD.cpp
+++ Prep/Leet/Hash/Easy/IntersectionOfTwoArrays-TBD.cpp
@@ -5,12 +5,50 @@
 
     https://leetcode.com/problems/intersection-of-two-arrays/
 */
-
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> hashmap;
-
         
+        int n1 = nums1.size();
+        int n2 = nums2.size(); 
+        vector<int> ans;
+        unordered_set<int> hashset; 
+        if(!n1 || !n2)
+            return ans;
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0, k = 0; 
+        
+        while(i < n1 && j < n2){
+            if(nums1[i] == nums2[j]){
+                hashset.insert(nums1[i]);
+                i++; j++;
+                cout << nums1[i] << " ";
+            } else if(nums1[i] < nums2[j]){
+                i++;
+            } else{
+                j++;
+            }
+        }
+        // Convert hashset to vector
+        ans.insert(ans.end(), hashset.begin(), hashset.end());
+
+        return ans;
+    }
+
+    // Without sorting 
+    vector<int> intersectionFaster(vector<int>& nums1, vector<int>& nums2) {
+        
+        // Add one of the arrays to a hashset
+        unordered_set<int> hashset(nums1.begin(), nums1.end());
+        
+        vector<int> res;
+        for (auto a : nums2)
+            if (hashset.count(a)) {
+                res.push_back(a);
+                hashset.erase(a);
+            }
+        return res;
     }
 };
--- Prep/Leet/Strings/Easy/NeedleHaystackStrStr.cpp
+++ Prep/Leet/Strings/Easy/NeedleHaystackStrStr.cpp
@@ -32,7 +32,7 @@ public:
 
 
     /*
-        KMP  
+        KMP - http://jakeboxer.com/blog/2009/12/13/the-knuth-morris-pratt-algorithm-in-my-own-words/1
     */
 
    public:
@@ -61,19 +61,20 @@ public:
         }
         return -1;
     }
-    private:
-        vector<int> kmpProcess(string& needle) {
-            int n = needle.length();
-            vector<int> lps(n, 0);
-            for (int i = 1, len = 0; i < n; ) {
-                if (needle[i] == needle[len]) {
-                    lps[i++] = ++len;
-                } else if (len) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i++] = 0;
-                }
+    
+    // Build the partial match table
+    vector<int> kmpProcess(string& needle) {
+        int n = needle.length();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n; ) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
             }
-            return lps;
         }
+        return lps;
+    }
 };

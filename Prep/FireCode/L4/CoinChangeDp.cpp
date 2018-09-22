36ce801f5318dc145156be53d10072fbeae16989
Reno <siq.samuel@gmail.com>
2018-09-22 23:34:40 +0530

    ' '

---
+++ Prep/FireCode/L4/CoinChangeDp.cpp
@@ -0,0 +1,12 @@
+#include "./../../../Include/Common.h"
+
+/*
+    Given an array containing the available denominations of coins in descending order, 
+    write a function to compute the number of possible ways of representing a monetary amount in cents. 
+*/
+
+
+int make_change(int coins[], int n, int amount){
+  
+  
+}
--- Prep/Leet/Hash/Easy/HappyNumber-TBD.cpp
+++ Prep/Leet/Hash/Easy/HappyNumber-TBD.cpp
@@ -4,10 +4,28 @@
     https://leetcode.com/problems/happy-number/
 
 */
-
 class Solution {
 public:
     bool isHappy(int n) {
+        unordered_set <int> hashset; 
         
+        int sum = 0; 
+        int number = n;
+        while (1) {
+            while(number) {
+                int res = number % 10;
+                number = number / 10;
+                
+                sum += (res * res);
+
+                if(sum  == 1)
+                    return true;
+            }
+            if(hashset.find(sum) != hashset.end())
+                    return false;
+
+            hashset.insert(sum);
+        }
+        return false;
     }
 };

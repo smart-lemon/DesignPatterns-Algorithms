22d98f72bf517726aaf68f825ad8980d77425f81
Reno <siq.samuel@gmail.com>
2018-09-26 00:13:25 +0530

    'There are two ways to solve this problem with and without modification of the tree'

---
+++ Prep/FireCode/L4/KthLargestNode-TBD.cpp
@@ -0,0 +1,13 @@
+#include "./../../../Include/Common.h"
+
+struct treeNode{
+    int value;              /* value of the node */
+    struct treeNode *left;  /* pointer to the left child node */
+    struct treeNode *right; /* pointer to the right child node */
+};
+
+
+treeNode* find_kth_largest(treeNode* root, int k) {}
+    
+    
+}

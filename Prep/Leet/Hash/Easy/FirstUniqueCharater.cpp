e2aa4a00a0c7a1ab62284a758fd165424fe6704f
Reno <siq.samuel@gmail.com>
2018-10-02 22:31:34 +0530

    'accepted 387'

---
+++ Prep/Leet/Hash/Easy/FirstUniqueCharater.cpp
@@ -0,0 +1,27 @@
+#include "./../../Include/Common.h"
+
+/*
+    387. First Unique Character in a String
+
+    https://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> hashmap; 
+        int ret = -1; 
+        // build a histogram 
+        for(char c: s){
+            hashmap[c]++; 
+        }
+       
+        
+        for(int i = 0; i < s.length(); i++){
+            if(hashmap[s[i]] == 1){
+                return i;
+            } 
+        }
+        return ret;         
+    }
+};

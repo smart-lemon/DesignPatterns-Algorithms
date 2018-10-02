122a54fdc2f310c7fc626978cf50a1132aee9624
Reno <siq.samuel@gmail.com>
2018-10-02 23:10:03 +0530

    'Intersection of two arrays again'

---
+++ Prep/Leet/Hash/Easy/IntersectionOfTwoArraysII.cpp
@@ -0,0 +1,30 @@
+#include "./../../Include/Common.h"
+
+/* 
+    350. Intersection of Two Arrays II
+    
+    https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+*/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> hashmap; 
+        vector<int> res; 
+        
+        vector<int> &longer  = nums1.size() >= nums2.size() ? nums1 : nums2; 
+        vector<int> &shorter = nums1.size() >= nums2.size() ? nums2 : nums1; 
+        
+        for(int i = 0; i < longer.size(); i++){
+            hashmap[longer[i]]++;
+        }
+        
+        for(int i = 0; i < shorter.size(); i++){
+            if( hashmap.find(shorter[i]) != hashmap.end() && hashmap[shorter[i]] > 0){
+                hashmap[shorter[i]]--;
+                res.push_back(shorter[i]);
+            }
+        }
+        return res;        
+    }
+};

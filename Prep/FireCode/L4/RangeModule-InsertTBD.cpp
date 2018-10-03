a0702b996851fb6fae7a445079a8e29336cd6106
Reno <siq.samuel@gmail.com>
2018-10-03 22:09:05 +0530

    'Merge interval implementation completed'

--- Prep/FireCode/L4/RangeModule-InsertTBD.cpp
+++ Prep/FireCode/L4/RangeModule-InsertTBD.cpp
@@ -23,7 +23,7 @@ class Interval {
         }
     };
 
-Interval merge_intervals(Interval iv1, Interval iv2){
+Interval merge_intervals(Interval &iv1, Interval &iv2){
     Interval merged;
     merged.start = min(iv1.start, iv2.start);
     merged.end = max(iv1.end, iv2.end);
@@ -31,44 +31,22 @@ Interval merge_intervals(Interval iv1, Interval iv2){
 }
 // Add any helper functions(if needed) above.
 vector<Interval> insert_range(vector<Interval> intervals_list, Interval insert) {
-    Interval prev, next = intervals_list[0];
-    auto it = intervals_list.begin();
+    vector<Interval> res; 
+    for(auto currIntvl : intervals_list){
+        if(currIntvl.end < insert.start) {
+            // --- ___
+            res.push_back(currIntvl);
 
-    // Insert at the start if needed
-    if(insert.end < next.start){
-        intervals_list.insert(it, insert);
-        return intervals_list;
-    } else if(insert.start < next.start && insert.end > next.start) {
-        Interval merged = merge_intervals(insert, next);
-        intervals_list[0] = merged;
-    }
+        } else if(insert.end < currIntvl.start){
+            // ___ ----
+            res.push_back(insert);
+            insert = currIntvl;
 
-    for(int i = 1; i < intervals_list.size(); i++){
-        prev = intervals_list[i - 1];
-        next = intervals_list[i];
-        if(insert.start >= prev.start && insert.start <= prev.end){
-            // ---___---
-            if(insert.end >= next.start && insert.end <= next.end){
-                Interval merged = merge_intervals(prev, next);
-                intervals_list[i - 1] = merged;                
-                intervals_list.erase(intervals_list.begin() + i);
-                continue;
-            // ---___  ---
-            } else if(insert.end < next.start ){
-                Interval merged = merge_intervals(prev, insert);
-                intervals_list[i - 1] = merged;
-                break;
-            }
-        // --- ___---
-        } else if(insert.start >= next.start && insert.start <= next.end){
-            Interval merged = merge_intervals(next, insert);
-            intervals_list[i] = merged;
-            break;
-            // --- ___ ---
-        } else if(insert.start > prev.start && insert.end < next.start) {
-            intervals_list.insert(it + i - 1, insert);
-            break;
+        } else if(insert.end >= currIntvl.start || currIntvl.end >= insert.start ){
+            // --=__ or __=--
+            insert = merge_intervals(insert, currIntvl);
         }
     }
-    return intervals_list;
+    res.push_back(insert);    
+    return res;
 }

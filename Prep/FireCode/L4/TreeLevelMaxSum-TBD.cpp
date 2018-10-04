9e16c36d4d800fc88409eb4c4f453a7c094f03c7
Reno <siq.samuel@gmail.com>
2018-10-05 00:56:17 +0530

    ''

--- Prep/FireCode/L4/TreeLevelMaxSum-TBD.cpp
+++ Prep/FireCode/L4/TreeLevelMaxSum-TBD.cpp
@@ -20,6 +20,33 @@ struct treeNode{
     Output :  2 
 */
 int find_max_sum_level(treeNode* root){
+    queue <treeNode*> q;
+    if(root == nullptr)
+        return -1;
     
+    q.push(root);
+    q.push(nullptr);
     
+    int max_sum = 0;
+    int sum = 0;
+    while(!q.empty()){
+        treeNode* node = q.front();
+        q.pop();
+        if(node == nullptr){
+            if(max_sum > sum)
+                max_sum = sum;
+            sum = 0;
+            q.push(nullptr);
+
+        } else {
+            sum += node->value;
+        }
+        
+        if(node->left)
+            q.push(node->left);
+        
+        if(node->right)
+            q.push(node->right);
+    };
+    return max_sum;     
 }

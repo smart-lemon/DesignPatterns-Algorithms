b8ddfb485b433f4327300a82e70b546a8b181b79
Reno <siq.samuel@gmail.com>
2018-10-15 01:09:29 +0530

    'Adding Insert question'

---
+++ Prep/Leet/Trees/Medium/BstInsert.cpp
@@ -0,0 +1,22 @@
+#include "./../../../../Include/Common.h"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/* 
+    701. Insert into a Binary Search Tree   
+    Insert the value into the BST. Return the root node of the BST after the insertion. 
+    
+    https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+*/
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+    }
+};

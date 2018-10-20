0d860ce13c69773c2159bc58ddf17aec11d45d03
Reno <siq.samuel@gmail.com>
2018-10-20 22:04:47 +0530

    ''

--- Data Structures/Trees/AvlTree.cpp
+++ Data Structures/Trees/AvlTree.cpp
@@ -1,29 +1,49 @@
 #include "./../../Include/CommonInc.h"
 
-typedef struct AvlTree {
-    AvlTree *left;
-    AvlTree *right; 
+typedef struct AvlNode {
+    AvlNode *left;
+    AvlNode *right; 
     int data; 
     int height;
 };
 
+int getHeight(AvlTree *node) {
+    if(!node) {
+        return -1;
+    } else {
+        node->height;
+    }
+}
 
 /*
     Left Left case
 
     Inserted into the left subtree of the left child of the violated node (X)
     
-    e.g 5 is violated,  
+    e.g 20 is violated,  
 
-            10                                         10 
-           /  \                                       /  \
-          5   15          Right Rotate at -10       -10  15
-         /                       ->                 /  \
-       -10                                        -20   5
+             /                                           /
+            20                                          15 
+           /  \                                       /    \
+          15   25          Right Rotate at 15        10     20
+         /  \                      ->               /  \   /  \
+        10  18                                    -10   5 18  25
        /
-    -20  <- Newly added
+    -10  <- Newly added
 */ 
 
+
+AvlNode *singleRotateRight(AvlNode *X) {
+    AvlNode *newX = X->left;
+    X->left = newX->right;
+    newX->right = X;
+
+    // Reconfigure the heights
+    
+    X->height = max(getHeight(X->left), getHeight(X->right)) + 1;
+    newX->height = max(getHeight(newX->left), getHeight(X->height)) + 1;
+    return newX;
+}
 
 
 /*

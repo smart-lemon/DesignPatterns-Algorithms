41891be1f699943d2f8c69681bacfe49222a4062
Reno <siq.samuel@gmail.com>
2018-10-21 18:15:48 +0530

    'Added printing of AVT tree'

--- Data Structures/Trees/AvlTree.cpp
+++ Data Structures/Trees/AvlTree.cpp
@@ -1,10 +1,12 @@
 #include "./../../Include/CommonInc.h"
 
-typedef struct AvlNode {
-    AvlNode *left;
-    AvlNode *right; 
-    int data; 
-    int height;
+class AvlNode {
+    
+    public:
+        AvlNode *left;
+        AvlNode *right; 
+        int data; 
+        int height;
 };
 
 int getHeight(AvlTree *node) {
@@ -45,23 +47,6 @@ AvlNode *singleRotateRight(AvlNode *X) {
 }
 
 
-/*
-    Left Right case
-
-    Inserted into the right subtree of the left child of the violated node (X)
-    
-    e.g 5 is violated,  
-
-            10                                         10                                  10 
-           /  \                                       /  \                                /  \
-          5   15        Left Rotate at -10           5   15      Right Rotate at -5     -5   15 
-         /                       ->                 /                  ->               /  \
-       -10                                        -5                                  -10   5
-          \                                       /
-          -5    <- Newly added                  -10
-*/ 
-
-
 
 /*
     Right Right case
@@ -93,6 +78,27 @@ AvlNode *singleRotateLeft(AvlNode *X) {
     return newX;
 }
 
+/*
+    Left Right case
+
+    Inserted into the right subtree of the left child of the violated node (Z)
+    
+    e.g 5 is violated,  
+
+            10                                         10                                  10 
+           /  \                                       /  \                                /  \
+          5   15        Left Rotate at -10           5   15      Right Rotate at -5     -5   15 
+         /                       ->                 /                  ->               /  \
+       -10                                        -5                                  -10   5
+          \                                       /
+          -5    <- Newly added                  -10
+*/ 
+
+AvlNode *doubleRotateLeftThenRight(AvlNode *Z) {
+    Z->left = singleRotateLeft(Z->left)
+
+    return singleRotateRight(Z);
+}
 
 /*
     Left Right case
@@ -111,11 +117,82 @@ AvlNode *singleRotateLeft(AvlNode *X) {
 */ 
 
 
+AvlNode *doubleRotateRightThenLeft(AvlNode *Z) {
+    Z->right = singleRotateRight(Z->left)
+
+    return singleRotateLeft(Z);
+}
 
 
+AvlNode *avlInsert(AvlNode *root, int data){
+    if(!root) {
+        root = new AvlNode;
+        root->left = root->right = nullptr;
+        root->data = data; 
+        root->height = 0;
+    } else {
+        if(data < root->data) {
+            root->left = avlInsert(root->left, data);
+
+            // Inserted in the left subtree. Check for imbalance
+            if(getHeight(root->left) - getHeight(root->right) == 2){
+                if(data < root->left->data)
+                    root = singleRotateRight(root);
+                else 
+                    root = doubleRotateLeftThenRight(root);
+            }
+        } else if(data > root->data){
+            root->right = avlInsert(root->right, data);
+            
+            // Inserted in the right subtree. Check for imbalance
+            if(getHeight(root->right) - getHeight(root->left) == 2){
+                if(data > root->right->data)
+                    root = singleRotateLeft(root);
+                else 
+                    root = doubleRotateRightThenLeft(root);
+            }
+        }
+        // Else the number is already on the tree. Now update the root's height 
+        root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
+    }
+    return root;
+}
+
+void printAvlTree(AvlNode *root, int level) {
+    if(root == nullptr)
+         return;
+
+    printAvlTree(root->right, level + 1);
+
+     if(level != 0){
+        for(int i = 0;i < level - 1; i++)
+           cout << "|\t";
+           cout <<  "|-------"  << root->data << endl;
+    } else {
+        cout << root->data;
+    }
+    printAvlTree(root->left, level + 1);
+
+}
 
 int testAvl() 
 {
-   
-   return 0;
+    AvlNode *root = avlInsert(nullptr, 20);
+
+    root = avlInsert(root, 15);
+    printAvlTree(root, 0);
+
+    root = avlInsert(root, 25);    
+    printAvlTree(root, 0);
+
+    root = avlInsert(root, 10);    
+    printAvlTree(root, 0);
+
+    root = avlInsert(root, 18);    
+    printAvlTree(root, 0);
+
+    root = avlInsert(root, -10);    
+    printAvlTree(root, 0);
+
+    return 0;
 }

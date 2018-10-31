277dc0e660245969de599b23a1d59809071ee4db
Reno <siq.samuel@gmail.com>
2018-11-01 00:59:43 +0530

    ''

--- Prep/Leet/Trees/Easy/IsBalancedBinaryTree.cpp
+++ Prep/Leet/Trees/Easy/IsBalancedBinaryTree.cpp
@@ -27,6 +27,17 @@ public:
     bool isBalanced(TreeNode* root) {
         return isBalancedUtil(root) != -1;
     }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    int height(TreeNode *root) {
+			if(root == nullptr)return 0;
+			return max(height(root->left), height(root->right)) + 1;
+		}
+		bool isBalanced(TreeNode* root) {
+			if(root == nullptr) return true;
+			return isBalanced(root->left) && isBalanced(root->right) 
+                   && abs(height(root->left) - height(root->right)) <= 1;
+		}
 };
 
 

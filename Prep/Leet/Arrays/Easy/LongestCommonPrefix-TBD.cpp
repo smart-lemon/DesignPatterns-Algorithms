615615a0adb62dca31478201bafa72c45796b716
Reno <siq.samuel@gmail.com>
2018-11-03 02:18:35 +0530

    ''

---
+++ Prep/Leet/Arrays/Easy/LongestCommonPrefix-TBD.cpp
@@ -0,0 +1,16 @@
+#include "./../../../../Include/Common.h"
+
+/* 
+    14. Longest Common Prefix
+    Write a function to find the longest common prefix string amongst an array of strings.
+
+    https://leetcode.com/problems/longest-common-prefix
+*/
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};

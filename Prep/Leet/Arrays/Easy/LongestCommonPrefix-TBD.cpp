16c9eec815300794d9213fab6c20716e3703ddcd
Reno <siq.samuel@gmail.com>
2018-11-04 20:19:10 +0530

    'Accepted -LP problem'

--- Prep/Leet/Arrays/Easy/LongestCommonPrefix-TBD.cpp
+++ Prep/Leet/Arrays/Easy/LongestCommonPrefix-TBD.cpp
@@ -7,10 +7,34 @@
     https://leetcode.com/problems/longest-common-prefix
 */
 
-
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
+        int mini = INT_MAX;
+        
+        // Find the smallest string len
+        int number_of_words = strs.size();
+        if(number_of_words == 0)
+            return "";
+        
+        for(int list = 0; list < number_of_words; list++){
+            mini = min(mini, (int)strs[list].size());
+        }
+        
+        string res = "";
+        // Compare the prefixes
+        for(int i = 0; i < mini; i++){
+            char current = strs[0][i];
+            int j;
+            for(j = 0; j < strs.size(); j++){
+                if(strs[j][i] != current)
+                    return res;
+            }
+            if(j == strs.size()){
+                res += current;
+            }
+        }
+        return res;
     }
 };

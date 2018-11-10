d144daa11ae868d9d7cf25a846cb0e5805bd90a5
Reno <siq.samuel@gmail.com>
2018-11-10 20:43:08 +0530

    'Pascals triangle space optimized for O(k)'

--- Prep/Leet/Dynamic Programming/Medium/PascalsTriangle-II.cpp
+++ Prep/Leet/Dynamic Programming/Medium/PascalsTriangle-II.cpp
@@ -26,9 +26,19 @@ public:
 };
 
 
-#include "./../../../../Include/Common.h"
-
-
 /*
     Space optimized for O(k)
 */
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex + 1, 1);
+        for(int i = 2; i <= rowIndex; i++){
+            for(int j = i - 1; j >= 1; j--){
+                res[j] = res[j] + res[j-1];
+            }
+        }
+        return res;
+    }
+};

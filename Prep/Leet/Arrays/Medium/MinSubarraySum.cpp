37393f286b92e8b877f08fd02f1d451e1d64a4f7
Reno <siq.samuel@gmail.com>
2018-11-10 20:12:20 +0530

    'No need of pre-sum'

--- Prep/Leet/Arrays/Medium/MinSubarraySum.cpp
+++ Prep/Leet/Arrays/Medium/MinSubarraySum.cpp
@@ -1,43 +1,15 @@
 
 #include "./../../../../Include/Common.h"
 /*
+    209. Minimum Size Subarray Sum 
+    Find the minimal length of a contiguous subarray of which the sum ≥ s. 
+
     https://leetcode.com/problems/minimum-size-subarray-sum
 */ 
 class Solution {
 public:
     
-    int min(int a, int b){
-        return a < b? a : b;
-    }
-    
-    int minSubArrayLen(int s, vector<int>& nums) {
-        int current_length = 0; 
-        int min_length = INT_MAX;
-        int end_index = 0;
-        int sum = 0; 
-        
-        for(int i = 0; i < nums.size(); i++){
-            
-            while(sum < s && end_index < nums.size()){
-                 sum += nums[end_index];                 
-                 end_index++;
-                 current_length++;
-             }
-            
-             current_length = end_index - i;
-            
-            if(current_length < min_length && sum >= s)
-                 min_length = current_length;
-            
-             sum -= nums[i];
-
-        }
-
-        // If there isn't a minimum length, return 0 instead.
-        return min_length == INT_MAX ? 0 : min_length;
-    }
-
-    int minSubArrayLenAlternative(int s, vector<int>& nums)
+    int minSubArrayLen(int s, vector<int>& nums)
     {
         int n = nums.size();
         int ans = INT_MAX;

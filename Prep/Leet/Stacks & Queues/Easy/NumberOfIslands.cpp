ac1b45f8980f2bc9e128fc26ab9dc2f2e35a57ea
Reno <siq.samuel@gmail.com>
2018-11-17 23:48:26 +0530

    'Adding new problem'

---
+++ Prep/Leet/Stacks & Queues/Easy/NumberOfIslands.cpp
@@ -0,0 +1,37 @@
+#include "./../../../../Include/Common.h"
+
+/*
+    Number of Islands : Given a 2d grid map of '1's (land) and '0's (water), 
+    count the number of islands
+
+
+*/
+
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+    }
+};
+
+
+/*
+
+Input:
+    11110
+    11010
+    11000
+    00000
+
+Output: 1
+
+Input:
+    11000
+    11000
+    00100
+    00011
+
+Output: 3
+
+*/

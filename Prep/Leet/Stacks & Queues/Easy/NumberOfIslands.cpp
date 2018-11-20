856ad57f6145f9618b5735222ccf91dcdbd5f3bc
Reno <siq.samuel@gmail.com>
2018-11-21 02:32:24 +0530

    'BFS works now'

--- Prep/Leet/Stacks & Queues/Easy/NumberOfIslands.cpp
+++ Prep/Leet/Stacks & Queues/Easy/NumberOfIslands.cpp
@@ -4,7 +4,7 @@
     Number of Islands : Given a 2d grid map of '1's (land) and '0's (water), 
     count the number of islands
 
-    
+    https://leetcode.com/problems/number-of-islands/
 */
 
 int dirs [][2] = {
@@ -31,21 +31,23 @@ class Solution {
             return islands;
         int c = grid[0].size();
 
-        bool doBfs = false;
-        vector<vector<bool>> visited(r, vector<bool> (c, false));
-        if(doBfs) {
+        bool doBfs = true;
 
+        // Do BFS 
+        if(doBfs) {
             // For every unvisited pixel in the map do a bfs
             for(int i = 0; i < r; i++) {
                 for(int j = 0; j < c; j++){
-                    if(grid[i][j] == '1' && !visited[i][j]){
+                    if(grid[i][j] == '1'){
                         islands++;
-                        bfs(grid, visited, i, j, r, c);
+                        bfs(grid, i, j, r, c);
                     }
                 }
             }
         } else {
-            // Do DFS
+            // Do DFS        
+            vector<vector<bool>> visited(r, vector<bool> (c, false));
+
               for (int i = 0; i < r; i++) {
                 for (int j = 0; j < c; j++) {
                     if (grid[i][j] == '1' && !visited[i][j]) {
@@ -54,26 +56,26 @@ class Solution {
                     }
                 }
             }
-        }
-        
+        }        
         return islands;
     }
     
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int r, int c){        
+    void bfs(vector<vector<char>>& grid, int i, int j, int r, int c){        
         
         queue<pair<int, int>> q;       
         q.push(make_pair(i, j));
 
-        while(q.size()){
+        while(!q.empty()){
             pair<int, int> location = q.front();
-            visited[location.first][location.second] = true; 
             q.pop();
             int new_r = 0, new_c = 0;
             for(int k = 0; k < 4; k++){
                 new_r = location.first  + dirs[k][0];
                 new_c = location.second + dirs[k][1];
                 if( check_boundary(new_r, new_c, r, c) && 
-                    grid[new_r][new_c] == '1' && !visited[new_r][new_c]){
+                    grid[new_r][new_c] == '1' ){                    
+                    // Instead of visited, modify the grid
+                    grid[new_r][new_c] = '0';
                     q.push(make_pair(new_r, new_c));
                 }
             }
@@ -87,8 +89,10 @@ class Solution {
             new_r = i + dirs[k][0];
             new_c = j + dirs[k][1];
 
-            if ( check_boundary(new_r, new_c, r, c) && grid[new_r][new_c] == '1' && !visited[new_r][new_c])
-                dfs(grid, visited, new_r, new_c, r, c);
+            if ( check_boundary(new_r, new_c, r, c) && 
+                 grid[new_r][new_c] == '1' && !visited[new_r][new_c]){
+                    dfs(grid, visited, new_r, new_c, r, c);
+            }
         }
     }
 };

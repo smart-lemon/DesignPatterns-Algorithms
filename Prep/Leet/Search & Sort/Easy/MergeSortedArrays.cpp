4e40bb1d9d02caf5700952b2be7d99a87fc6fe36
Reno <siq.samuel@gmail.com>
2018-12-26 00:25:31 +0530

    'TF problems and solutions'

---
+++ Prep/Leet/Search & Sort/Easy/MergeSortedArrays.cpp
@@ -0,0 +1,24 @@
+#include "./../../../../Include/Common.h"
+
+#include "./../../../../Include/Common.h"
+
+/*
+    Given two sorted arrays, merge them
+
+*/
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+    }
+};
+
+/*
+    Input:
+    nums1 = [1,2,3,0,0,0], m = 3
+    nums2 = [2,5,6],       n = 3
+
+    Output: [1,2,2,3,5,6
+*/
